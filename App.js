e0f3236bf628da65395422b5c1386ca30b2b1713
Richard Shea <rshea@thecubagroup.com>
2021-11-30 21:30:52 +1300

    Working version of the camera app. Works in a web context yet to be tested as an app

--- App.js
+++ App.js
@@ -1,12 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import { logger } from "react-native-logs";
 
 export default function App() {
+  var rnlog = logger.createLogger();
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+
+  const takePicture = async () => {
+    rnlog.debug('takePicture A')
+    if(camera){
+      rnlog.debug('takePicture B')
+      const data = await camera.takePictureAsync(null);
+      rnlog.debug(data.uri)
+      rnlog.debug('takePicture C')
+      setImage(data.uri)
+      rnlog.debug('takePicture D')
+    }
+  }
+
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.cameraContainer}>
+          <Camera 
+            ref={ref => setCamera(ref)}
+            style={styles.camera} 
+            type={type} 
+            ratio={'1:1'} 
+          />
+      </View>        
+
+      <Button
+        style={styles.button}
+        title="Flip Image"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}>
+        
+      </Button>
+
+      <Button title="Take Picture" onPress={() => takePicture()} />
+      {image && <Image source={{uri: image}} style={{flex:1}} />}
+
     </View>
   );
 }
@@ -14,8 +70,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange'
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
+
